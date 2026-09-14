7615e022b4994137e21a79f88b675e41d043151f
Filippo Turazzi <filippo.turazzi.luch@gmail.com>
2026-09-14 10:42:07 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import Header from './componentes/Header/index.jsx';
+import Apresentacao from './componentes/Apresentacao/index.jsx';
+import ComoFunciona from './componentes/ComoFunciona/index.jsx'
+
+
+function App(){
+  return(
+    <>
+      <Header></Header>
+      <Apresentacao></Apresentacao>
+      <ComoFunciona></ComoFunciona>
+    </>
+  )
+}
+
+export default App;
